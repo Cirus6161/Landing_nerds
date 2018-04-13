3996107e16018e4976fe1e203ca77bb65275c518
Unknown <cirus6161@gmail.com>
2018-04-13 16:46:36 +0300

    commit16

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,71 @@
 $(document).ready(function(){
+
+    /*Slick-slider*/
     $('.sl').slick({
         dots: true
     });
+
+
+
+    
+
+    /*phone-mask*/
+    $(".phone").mask("+9(999)999-9999");
+
+
+    /*jquery-validation*/
+    $('#common_form1').validate({
+        rules:{
+            name:{
+                required: true
+            },
+            cellphone:{
+                required: true
+            },
+            email:{
+                required: true,
+                email: true
+            }
+        },
+        submitHandler: function(form){
+            $.ajax({
+                url: "mail.php",
+                type: "POST",
+                data: $(form).serializeArray(),
+                success: function (response) {
+                    form.reset();
+                }
+            })
+        }
+    });
+
+
+    $('#common_form2').validate({
+        rules:{
+            name:{
+                required: true
+            },
+            cellphone:{
+                required: true
+            },
+            email:{
+                required: true,
+                email: true
+            }
+        },
+        submitHandler: function(form){
+            $.ajax({
+                url: "mail.php",
+                type: "POST",
+                data: $(form).serializeArray(),
+                success: function (response) {
+                    form.reset();
+                }
+            })
+        }
+    });
+
+
 });
+
+
