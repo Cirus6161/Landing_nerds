44814148e03db2cf33949bef3bb6ad68bb13bd94
Unknown <cirus6161@gmail.com>
2018-04-18 00:00:24 +0300

    commit 24

---
+++ dist/js/script.js
@@ -0,0 +1,91 @@
+$(document).ready(function(){
+
+    /*Slick-slider*/
+    $('.sl').slick({
+        dots: true
+    });
+
+
+
+    $(document).on('click','.about-video__poster',function(e) {
+
+        var $video = $('#inner-video'),
+            src = $video.attr('src');
+
+        $('.about-video__poster').hide();
+        $video.attr('src', src + '&autoplay=1');
+    });
+
+    $(document).on('click','.services-video__poster',function(e) {
+
+        var $video = $('#inner-video2'),
+            src = $video.attr('src');
+
+        $('.services-video__poster').hide();
+        $video.attr('src', src + '&autoplay=1');
+    });
+
+
+
+
+
+    /*phone-mask*/
+    $(".phone").mask("+9(999)999-9999");
+
+
+    /*jquery-validation*/
+    $('#common_form1').validate({
+        rules:{
+            name:{
+                required: true
+            },
+            cellphone:{
+                required: true
+            },
+            email:{
+                required: true,
+                email: true
+            }
+        },
+        submitHandler: function(form){
+            $.ajax({
+                url: "mail.php",
+                type: "POST",
+                data: $(form).serializeArray(),
+                success: function (response) {
+                    form.reset();
+                }
+            })
+        }
+    });
+
+
+    $('#common_form2').validate({
+        rules:{
+            name:{
+                required: true
+            },
+            cellphone:{
+                required: true
+            },
+            email:{
+                required: true,
+                email: true
+            }
+        },
+        submitHandler: function(form){
+            $.ajax({
+                url: "mail.php",
+                type: "POST",
+                data: $(form).serializeArray(),
+                success: function (response) {
+                    form.reset();
+                }
+            })
+        }
+    });
+
+
+});
+
+
